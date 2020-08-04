5ded472cce2590fe5b1efafb7b8f902f8ba4b9b3
chDame <ch.dame@gmail.com>
2020-08-04 18:22:15 +0200

    come back to normal backend

--- env.tsx
+++ env.tsx
@@ -1,4 +1,4 @@
 export const env = {
-	backend: 'http://fcde8e593ac8.ngrok.io'
-	//backend: 'https://back.fabulexie.fr'
+	//backend: 'http://fcde8e593ac8.ngrok.io'
+	backend: 'https://back.fabulexie.fr'
 }
